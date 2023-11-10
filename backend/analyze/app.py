449742ad3f65681cf827474e96a2412edaa712d6
PatrikHolopainen <oncerecursive@gmail.com>
2023-11-10 21:16:21 +0200

    templavideot

--- backend/analyze/app.py
+++ backend/analyze/app.py
@@ -1,6 +1,36 @@
 import json
+from pydub import AudioSegment
+import io
+import os
+import sys
+print(sys.executable)
 
-def lambda_handler(event, context):
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def lambda_handler(event, context):    
+    
+    API_TOKEN = os.getenv('API_TOKEN')
+
+    print(API_TOKEN)
+
+    def extract_audio_from_mp4(mp4_file_path, output_audio_path):
+        # Load the mp4 file
+        video = AudioSegment.from_file(mp4_file_path, "mp4")
+
+        # Export the audio
+        video.export(output_audio_path, format="mp3")
+
+    # Example usage
+
+    file = io.BytesIO()
+
+    extract_audio_from_mp4("video1.mp4", file)
+
+
     """Sample pure Lambda function
 
     Parameters
