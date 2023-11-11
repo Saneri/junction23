0bc6c920520c9d1e4709d7043f415315e2053069
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2023-11-11 18:26:21 +0200

    move python deps to layer requirements.txt

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,10 +1,10 @@
 readonly PROJECT_NAME=junction23-backend
 
 sam build
-sam deploy  \
+sam deploy \
     --region eu-west-1 \
     --stack-name ${PROJECT_NAME} \
     --profile santeri-dev \
     --capabilities CAPABILITY_IAM \
     --no-confirm-changeset \
-    --s3-bucket ${PROJECT_NAME}-sam \
+    --s3-bucket ${PROJECT_NAME}-sam
